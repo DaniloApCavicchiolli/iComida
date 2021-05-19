8d7ce744bb573ccc626a58fe1a1efb9bf3a960b9
DaniloApCavicchiolli <daniloap.cavicchiolli@gmail.com>
2021-05-19 11:52:19 -0300

    Continuação do projeto

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT
 //Parse conteúdo JSON
 app.use(express.json())//Verifica se o conteúdo que está sendo mandado, ele consegue interpretar como json
 
-//Caminho inicial
+//##### Caminho inicial #####
 //Caso venha alguma coisa via get() no diretório raiz. Vou ter o tratamento (requisição e resposta) **Sempre vem um (request, response)**
 app.get('/', (req, res) => {
    // const idiomas = req.headers['accept-language'] //idiomas aceito no navegador
--- routes/Categoria.js
+++ routes/Categoria.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+//express-validator = um auxiliador para fazer validações
+const { check, validationResult } = require('express-validator') //Importação do express-validator
 const Categoria = require('../model/Categoria')
 
 /************************************************** 
@@ -19,6 +21,100 @@ router.get('/', async(req, res) => {
             errors: [{message: ' Não foi possível obter as categorias!'}]
         })
     }
+})
+
+/************************************************** 
+ * Lista uma categoria pelo id
+ * GET /categorias/:id
+***************************************************/
+//(:) = Quando é colocado dois pontos, ele intende que é um parametro que eu vou receber
+// findById() = Localiza pelo id  
+router.get('/:id', async(req, res) => {
+    try {
+        const categoria = await Categoria.findById(req.params.id) // Equivale ao "Select * from" de um Banco de dados
+        res.json(categoria)
+    }catch (err) {
+        res.status(500).send({
+            errors: [{message:  `Não foi possível obter a categoria com o id ${req.params.id}`}]
+        })
+    }
+})
+
+/************************************************** 
+ * Inclui uma nova categoria
+ * POST /categorias
+***************************************************/
+const validaCategoria = [
+    // Estou checando se o campo nome não é vazio
+    check("nome", "Nome da categoria é obrigatório.").not().isEmpty(),
+    // Estou verificando se o campo status está destro dos parametros 'ativo' ou 'inativo'
+    check("status", "Informe um status válido para a categoria.").isIn(['ativo','inativo'])
+]
+router.post('/', validaCategoria, async(req, res) => {
+    //Estou fazendo a validação dessa requisição
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){ //Verifica se o array não está vazio
+        return res.status(400).json({
+            errors: errors.array()// exibe os erros
+        })
+    }
+    //Verificar se a categoria já existe
+    const { nome } = req.body// Vai no corpo da requisição e pega o nome
+    let categoria = await Categoria.findOne({nome})//Vou aguardar ele ir até a categoria o findOne(localizarUm) pelo {nome}
+    if(categoria){
+        return res.status(200).json({
+            errors: [{message: "Já existe uma categoria com o nome informado!"}]
+        })
+    }
+    try{
+        let categoria = new Categoria(req.body)
+        await categoria.save()
+        res.send(categoria)
+    }catch (err){
+        return res.status(500).json({
+            errors: [{message: `Erro ao salvar a categoria: ${err.message}`}]
+        })
+    }
+
+})
+
+/************************************************** 
+ * Remove uma categoria
+ * DELETE /categorias/:id
+***************************************************/
+router.delete('/:id', async(req, res) => {
+    await Categoria.findByIdAndRemove(req.params.id)
+    .then(categoria => {
+        res.send({message: `Categoria ${categoria.nome} removida com sucesso! `})
+    }).catch(err => {
+        return res.status(500).send({
+            errors: [{message: `Não foi possivel apagar a categoria com o id: ${req.params.id}`}]
+        })
+    })
+})
+
+/************************************************** 
+ * Edita a categoria
+ * PUT /categorias
+***************************************************/
+router.put('/', validaCategoria, async(req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    let dados = req.body 
+    await Categoria.findByIdAndUpdate(req.body._id, {
+        $set: dados
+    }, {new: true})//true = mostra os dados que foram alterados
+    .then(categoria => {
+        res.send({message: `Categoria ${categoria.nome} alterada com sucesso!`})
+    }).catch (err => {
+        return res.status(500).send({
+            errors: [{message: `Não foi possível alterar a categoria com o id: ${req.body._id}`}]
+        })
+    })
 })
 
 module.exports = router
